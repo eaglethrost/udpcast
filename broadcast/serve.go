53d58a2b75a213e62de8a8a276068c7d5bf0b6b9
eaglethrost <dimazanugrah12@gmail.com>
2023-01-20 12:28:22 +0700

    working server

--- broadcast/serve.go
+++ broadcast/serve.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net"
-	"time"
 )
 
 /*
@@ -33,8 +32,7 @@ func main() {
 	if err != nil {
 		err = fmt.Errorf("listenudp :%s", addr.String())
 	}
-	udpl.Close()
-
+	defer udpl.Close()
 	fmt.Printf("Connected...  %s\n", addr)
 
 	// TESTING -----
@@ -43,35 +41,10 @@ func main() {
 		IP:   net.IP{0xFF, 0xFF, 0xFF, 0xFF},
 		Port: 5000,
 	}
-	msg := []byte("siuuu")
-	for {
-		time.Sleep(2 * time.Second)
-		udpl.WriteToUDP(msg, bcast_addr)
-	}
+	msg := []byte("hello world")
+	udpl.WriteToUDP(msg, bcast_addr)
 	// TESTING ------
 
 	// 2. Prepare UDP pakcet
-
 	// 3. Broadcast packet
 }
-
-// package main
-
-// import (
-// 	"net"
-// )
-
-// func main() {
-// 	pc, err := net.ListenPacket("udp4", ":5000")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer pc.Close()
-
-// 	addr, err := net.ResolveUDPAddr("udp4", "192.168.7.255:8829")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	pc.WriteTo([]byte("data to transmit"), addr)
-// }
