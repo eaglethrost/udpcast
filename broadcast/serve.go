ac1e0a6ddcb4b8e4e67c024337071002bacfbcdc
eaglethrost <dimazanugrah12@gmail.com>
2023-01-20 09:53:04 +0700

    initial listen & serve

---
+++ broadcast/serve.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+/*
+	Program to do udp broadcast with data of 1444 bytes
+	First 4 byte offset is sequence of packet
+*/
+
+const (
+	PKT_MAX_LEN = 1444
+	PORT        = 5000
+)
+
+func main() {
+
+	// 1. Create UDP Connection
+
+	// addr = *net.UDPAddr
+	conn, err := net.Dial("udp", "192.168.2.20:5000")
+	if err != nil {
+		err = fmt.Errorf("can't dial")
+	}
+	addr := conn.LocalAddr().(*net.UDPAddr)
+	conn.Close()
+
+	udpl, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		err = fmt.Errorf("listenudp :%s", addr.String())
+	}
+	udpl.Close()
+
+	fmt.Printf("Connected...  %s\n", addr)
+	for {
+		time.Sleep(2 * time.Second)
+	}
+
+	// 2. (TEST) Write to connection
+	bcast_addr := &net.UDPAddr{
+		IP:   net.IP{0xFF, 0xFF, 0xFF, 0xFF},
+		Port: 5000,
+	}
+	msg := []byte("hello world!")
+	udpl.WriteToUDP(msg, bcast_addr)
+
+	// 2. Prepare UDP pakcet
+
+	// 3. Broadcast packet
+}
---
+++ listen/listen.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("listen script")
+
+	// 1. TAP INTO CONNECTION
+
+	// 2. LISTEN TO BROADCASTS
+
+	// 3. PRINT BROADCASTED MESSAGE
+}
