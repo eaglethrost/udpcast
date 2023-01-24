932a3e7ba73f4122245a40569c8b84fe599af7eb
eaglethrost <dimazanugrah12@gmail.com>
2023-01-24 10:50:57 +0700

    packaged data

--- broadcast/serve.go
+++ broadcast/serve.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
 	"net"
+	"os"
+	"time"
 )
 
 /*
@@ -14,6 +17,13 @@ const (
 	PKT_MAX_LEN = 1444
 	PORT        = 5000
 )
+
+func Min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
 
 func main() {
 
@@ -37,14 +47,44 @@ func main() {
 
 	// TESTING -----
 	// 2. (TEST) Write to connection
+	// msg := []byte("hello world")
+	// udpl.WriteToUDP(msg, bcast_addr)
+	// TESTING ------
+
+	// 2. Prepare UDP pakcet
 	bcast_addr := &net.UDPAddr{
 		IP:   net.IP{0xFF, 0xFF, 0xFF, 0xFF},
 		Port: 5000,
 	}
-	msg := []byte("hello world")
-	udpl.WriteToUDP(msg, bcast_addr)
-	// TESTING ------
+	// 2.1 Get data
+	fileBytes, err := os.ReadFile("test.zip")
+	if err != nil {
+		fmt.Printf("Error opening file \n")
+	}
 
-	// 2. Prepare UDP pakcet
-	// 3. Broadcast packet
+	// 2.2 Divide data into packets of 1444 bytes
+	fileLen := len(fileBytes)
+	no_of_pkts := fileLen / 1440
+	fmt.Printf("Size: %d bytes Packets: %d\n", fileLen, no_of_pkts)
+
+	for i := 0; i <= no_of_pkts; i++ {
+		// 2.3 Set first 4 bytes as sequence number
+		pkt := make([]byte, 1444)
+		binary.BigEndian.PutUint16(pkt[:2], uint16(i+1))
+		binary.BigEndian.PutUint16(pkt[2:4], uint16(no_of_pkts))
+
+		// 2.4 Merge bytes
+		edge := Min(1440*i+1440, fileLen)
+		start := 1440 * i
+		fmt.Printf("Seq %d) %d : %d\n", i, start, edge)
+		copy(pkt[4:], fileBytes[start:edge])
+
+		// 3. Broadcast packet
+		udpl.WriteToUDP(pkt, bcast_addr)
+
+		time.Sleep(time.Millisecond * 200)
+		fmt.Printf("Sent packet:")
+		// fmt.Println(pkt)
+	}
+	fmt.Println("Done sending")
 }
